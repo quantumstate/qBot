00ffd4bc856baccd8371043c26ce054a1b8cc7dc
Jonathan Waller <jonathanmarkwaller@gmail.com>
2011-10-05 23:19:07 +0100

    Removed testbot description

--- qbot.js
+++ qbot.js
@@ -1,36 +1,3 @@
-/*
- * This is a primitive initial attempt at an AI player.
- * The design isn't great and maybe the whole thing should be rewritten -
- * the aim here is just to have something that basically works, and to
- * learn more about what's really needed for a decent AI design.
- *
- * The basic idea is we have a collection of independent modules
- * (EconomyManager, etc) which produce a list of plans.
- * The modules are mostly stateless - each turn they look at the current
- * world state, and produce some plans that will improve the state.
- * E.g. if there's too few worker units, they'll do a plan to train
- * another one. Plans are discarded after the current turn, if they
- * haven't been executed.
- *
- * Plans are grouped into a small number of PlanGroups, and for each
- * group we try to execute the highest-priority plans.
- * If some plan groups need more resources to execute their highest-priority
- * plan, we'll distribute any unallocated resources to that group's
- * escrow account. Eventually they'll accumulate enough to afford their plan.
- * (The purpose is to ensure resources are shared fairly between all the
- * plan groups - none of them should be starved even if they're trying to
- * execute a really expensive plan.)
- */
-
-/*
- * Lots of things we should fix:
- *
- *  * Find entities with no assigned role, and give them something to do
- *  * Keep some units back for defence
- *  * Consistent terminology (type vs template etc)
- *  * ...
- *
- */
 
 function QBotAI(settings) {
 	BaseAI.call(this, settings);

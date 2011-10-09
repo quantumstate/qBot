68bbdebd50d7e4fdc6e04f10a39a0f656d3a452e
quantumstate <jonathanmarkwaller@gmail.com>
2011-10-09 22:20:51 +0100

    Added functions to extend EntityTemplate (from 0 AD trac #991) temporarily here until the patch is applied to svn

--- entity-extend.js
+++ entity-extend.js
@@ -1,3 +1,19 @@
 Entity.prototype.deleteMetadata = function(id) {
 	delete this._ai._entityMetadata[this.id()];
 };
+
+EntityTemplate.prototype.attackStrengths = function() {
+	if (!this._template.Attack)
+		return undefined;
+	var ret = {};
+	for ( var type in this._template.Attack)
+		ret[type] = this._template.Attack[type];
+	return ret;
+};
+
+EntityTemplate.prototype.armorStrengths = function() {
+	if (!this._template.Armor)
+		return undefined;
+
+	return ret = this._template.Armor;
+};
